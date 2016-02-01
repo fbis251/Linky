bcea5241981a3e0bd4d86e530a233d103480c467
Fernando Barillas <fbis251@mailbox.org>
2016-01-31 17:50:18 -0800

    Implement SwipeRefreshLayout in LinksList

--- app/src/main/java/com/fernandobarillas/linkshare/LinksListActivity.java
+++ app/src/main/java/com/fernandobarillas/linkshare/LinksListActivity.java
@@ -3,13 +3,13 @@ package com.fernandobarillas.linkshare;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.fernandobarillas.linkshare.adapters.LinksAdapter;
 import com.fernandobarillas.linkshare.api.LinkShare;
@@ -26,6 +26,7 @@ public class LinksListActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = "MainActivity";
     Retrofit mRetrofit;
+    SwipeRefreshLayout mSwipeRefreshLayout;
     RecyclerView mRecyclerView;
     LinksAdapter mLinksAdapter;
 
@@ -39,7 +40,6 @@ public class LinksListActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
@@ -51,6 +51,15 @@ public class LinksListActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
+
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.links_swipe_refresh_layout);
+        mSwipeRefreshLayout.setProgressViewOffset(false, 0, 100);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getLinks();
             }
         });
 
@@ -73,19 +82,32 @@ public class LinksListActivity extends AppCompatActivity {
                 for (String url : urls) {
                     Log.e(LOG_TAG, "" + url);
                 }
+                showSnackBar(String.format("Downloaded %d links(s)", urls.size()));
                 mLinksAdapter = new LinksAdapter(getApplicationContext(), urls);
                 mRecyclerView.setAdapter(mLinksAdapter);
+                mSwipeRefreshLayout.setRefreshing(false);
             }
 
             @Override
             public void onFailure(Throwable t) {
                 Log.e(LOG_TAG, "onResponse: Error during call" + t.getLocalizedMessage());
-                showToast(t.getLocalizedMessage());
+                showError(t.getLocalizedMessage());
+                mSwipeRefreshLayout.setRefreshing(false);
             }
         });
     }
 
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    private void showError(String message) {
+        showSnackBar(message, true);
     }
+
+    private void showSnackBar(String message) {
+        showSnackBar(message, false);
+    }
+
+    private void showSnackBar(String message, boolean indefinite) {
+        int length = indefinite ? Snackbar.LENGTH_INDEFINITE : Snackbar.LENGTH_LONG;
+        Snackbar.make(mRecyclerView, message, length).setAction("Action", null).show();
+    }
+
 }
