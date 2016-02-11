05e9af1aafe1ed00b1e4dda30ae4485e0c2b7fb0
Fernando Barillas <fbis251@mailbox.org>
2016-02-10 16:36:01 -0800

    Constants: Remove hard-set API base URL

--- app/src/main/java/com/fernandobarillas/linkshare/configuration/Constants.java
+++ app/src/main/java/com/fernandobarillas/linkshare/configuration/Constants.java
@@ -5,8 +5,6 @@ package com.fernandobarillas.linkshare.configuration;
  */
 public class Constants {
 
-    public static final String API_BASE_URL = "https://r.0fb.xyz";
-
     public static final boolean USE_HTTP_PROXY = false;
 
     public static final String HTTP_PROXY_ADDRESS = "192.168.1.2";
