75b875da1b963524b18430e1f5402826fc5963ea
Fernando Barillas <fbis251@mailbox.org>
2016-02-10 16:28:34 -0800

    Add LoginActivity based on Android sample Activity

---
+++ app/src/main/java/com/fernandobarillas/linkshare/LoginActivity.java
@@ -0,0 +1,252 @@
+package com.fernandobarillas.linkshare;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.fernandobarillas.linkshare.api.LinkShare;
+import com.fernandobarillas.linkshare.api.ServiceGenerator;
+import com.fernandobarillas.linkshare.configuration.AppPreferences;
+import com.fernandobarillas.linkshare.exceptions.InvalidApiUrlException;
+import com.fernandobarillas.linkshare.models.LoginRequest;
+import com.fernandobarillas.linkshare.models.LoginResponse;
+
+import java.net.URL;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "LoginActivity";
+
+    AppPreferences mPreferences;
+    LinkShare mLinkShare;
+
+    // UI references.
+    private EditText mApiUrlView;
+    private EditText mUsernameView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setupActionBar();
+        // Set up the login form.
+        mApiUrlView = (EditText) findViewById(R.id.api_url);
+        mUsernameView = (EditText) findViewById(R.id.username);
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mSignInButton = (Button) findViewById(R.id.sign_in_button);
+        mSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+
+        mPreferences = new AppPreferences(getApplicationContext());
+    }
+
+    /**
+     * Set up the {@link android.app.ActionBar}, if the API is available.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void setupActionBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // Show the Up button in the action bar.
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form. If there are form
+     * errors (invalid email, missing fields, etc.), the errors are presented and no actual login
+     * attempt is made.
+     */
+    private void attemptLogin() {
+        // Reset errors.
+        mApiUrlView.setError(null);
+        mUsernameView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String apiUrl = mApiUrlView.getText().toString();
+        String username = mUsernameView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_field_required));
+            focusView = mPasswordView;
+            cancel = true;
+        } else if (!isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid username
+        if (TextUtils.isEmpty(username)) {
+            mUsernameView.setError(getString(R.string.error_field_required));
+            focusView = mUsernameView;
+            cancel = true;
+        } else if (!isUsernameValid(username)) {
+            mUsernameView.setError(getString(R.string.error_invalid_username));
+            focusView = mUsernameView;
+            cancel = true;
+        }
+
+        // Check for a valid API URL
+        if (TextUtils.isEmpty(apiUrl)) {
+            mApiUrlView.setError(getString(R.string.error_field_required));
+            focusView = mApiUrlView;
+            cancel = true;
+        } else if (!isApiUrlValid(apiUrl)) {
+            mApiUrlView.setError(getString(R.string.error_invalid_api_url));
+            focusView = mApiUrlView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            doLogin(apiUrl, username, password);
+        }
+    }
+
+    private void serviceSetup(URL apiUrl) throws InvalidApiUrlException {
+        Log.v(LOG_TAG, "serviceSetup()");
+        mLinkShare = ServiceGenerator.createService(LinkShare.class, apiUrl);
+    }
+
+    private void doLogin(final String apiUrl, final String username, final String password) {
+        LoginRequest loginRequest = new LoginRequest(username, password);
+        Call<LoginResponse> loginCall = mLinkShare.login(loginRequest);
+        loginCall.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onFailure(Throwable t) {
+                Log.v(LOG_TAG, "onFailure() called with: " + "t = [" + t + "]");
+                Log.e("LinksListActivity", t.getLocalizedMessage());
+                mUsernameView.setError(getString(R.string.error_incorrect_username_or_password));
+                mPasswordView.setError(getString(R.string.error_incorrect_username_or_password));
+            }
+
+            @Override
+            public void onResponse(Response<LoginResponse> response) {
+                Log.v(LOG_TAG, "onResponse() called with: " + "response = [" + response + "]");
+                showProgress(false);
+                if (response.isSuccess()) {
+                    LoginResponse loginResponse = response.body();
+                    if (loginResponse.isSuccess()) {
+                        String refreshToken = loginResponse.getRefreshToken();
+                        if (!TextUtils.isEmpty(refreshToken)) {
+                            mPreferences.setApiUrl(apiUrl);
+                            mPreferences.setRefreshToken(refreshToken);
+                            mPreferences.setUsername(username);
+                            Log.i(LOG_TAG, "Login completed, starting LinksListActivity");
+                            startActivity(new Intent(getApplicationContext(), LinksListActivity.class));
+                            finish();
+                            return;
+                        }
+                    }
+                }
+
+                mUsernameView.setError(getString(R.string.error_incorrect_username_or_password));
+                mPasswordView.setError(getString(R.string.error_incorrect_username_or_password));
+            }
+        });
+    }
+
+    private boolean isApiUrlValid(String apiUrl) {
+        try {
+            serviceSetup(new URL(apiUrl));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private boolean isUsernameValid(String username) {
+        return username.length() >= 4;
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() >= 4;
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+}
+
