14594a871ff44e55bac5d97ac1c9580f86a42b6c
Fernando Barillas <fbis251@mailbox.org>
2016-09-21 00:33:35 -0700

    Update LoginResponse model to use authString field

--- app/src/main/java/com/fernandobarillas/linkshare/models/LoginResponse.java
+++ app/src/main/java/com/fernandobarillas/linkshare/models/LoginResponse.java
@@ -4,28 +4,30 @@ package com.fernandobarillas.linkshare.models;
  * Created by fb on 2/2/16.
  */
 public class LoginResponse {
-    boolean successful;
-    String  authToken;
-    String  username;
+    public static final long INVALID_USER_ID = -1;
+
+    private long userId = INVALID_USER_ID;
+    private String authString;
+    private String username;
 
     @Override
     public String toString() {
         return "LoginResponse{" +
-                "successful=" + successful +
-                ", authToken='" + authToken + '\'' +
+                "userId=" + userId +
+                ", authString='" + authString + '\'' +
                 ", username='" + username + '\'' +
                 '}';
     }
 
-    public String getAuthToken() {
-        return authToken;
+    public String getAuthString() {
+        return authString;
+    }
+
+    public long getUserId() {
+        return userId;
     }
 
     public String getUsername() {
         return username;
-    }
-
-    public boolean isSuccessful() {
-        return successful;
     }
 }
