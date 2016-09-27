ac5724cea2b2c70acd43268c91283f9c1e741787
Fernando Barillas <fbis251@mailbox.org>
2016-09-27 11:54:38 -0700

    EditLinkActivity: Use AutoCompleteTextView for category input

--- app/src/main/java/com/fernandobarillas/linkshare/activities/EditLinkActivity.java
+++ app/src/main/java/com/fernandobarillas/linkshare/activities/EditLinkActivity.java
@@ -1,22 +1,15 @@
 package com.fernandobarillas.linkshare.activities;
 
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.text.InputFilter;
 import android.text.Spanned;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.Switch;
 
 import com.fernandobarillas.linkshare.R;
@@ -26,25 +19,23 @@ import com.fernandobarillas.linkshare.models.AddLinkResponse;
 import com.fernandobarillas.linkshare.models.Link;
 
 import java.io.IOException;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class EditLinkActivity extends BaseLinkActivity
-        implements AdapterView.OnItemSelectedListener {
+public class EditLinkActivity extends BaseLinkActivity {
 
     public static final String EXTRA_LINK_ID = "link_id";
 
-    private static final int ADD_NEW_CATEGORY_POSITION = 1;
-
-    private EditText mTitleEditText;
-    private EditText mUrlEditText;
-    private Spinner  mCategoriesSpinner;
-    private Switch   mArchivedSwitch;
-    private Switch   mFavoriteSwitch;
+    private EditText             mTitleEditText;
+    private EditText             mUrlEditText;
+    private AutoCompleteTextView mCategoryEditText;
+    private Switch               mArchivedSwitch;
+    private Switch               mFavoriteSwitch;
 
     private Link mIntentLink;
 
@@ -85,7 +76,7 @@ public class EditLinkActivity extends BaseLinkActivity
 
         mTitleEditText = binding.editLinkTitle;
         mUrlEditText = binding.editLinkUrl;
-        mCategoriesSpinner = binding.editLinkCategoriesSpinner;
+        mCategoryEditText = binding.editLinkCategory;
         mArchivedSwitch = binding.editLinkArchivedSwitch;
         mFavoriteSwitch = binding.editLinkFavoriteSwitch;
 
@@ -107,21 +98,10 @@ public class EditLinkActivity extends BaseLinkActivity
 
         mTitleEditText.setFilters(noReturnsInputFilter);
 
-        populateCategoriesSpinner(null);
-        mCategoriesSpinner.setOnItemSelectedListener(this);
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-        if (position == ADD_NEW_CATEGORY_POSITION) {
-            new CategoryDialog(EditLinkActivity.this).show();
-        }
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-        // Leave category as is
-
+        List<String> categoryList = new ArrayList<>(mLinkStorage.getCategories());
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, categoryList);
+        mCategoryEditText.setAdapter(adapter);
     }
 
     @Override
@@ -132,39 +112,6 @@ public class EditLinkActivity extends BaseLinkActivity
             outState.putLong(EXTRA_LINK_ID, mIntentLink.getLinkId());
         }
         super.onSaveInstanceState(outState);
-    }
-
-    private void populateCategoriesSpinner(@Nullable final String newCategory) {
-        Log.v(LOG_TAG, "populateCategoriesSpinner() called with: "
-                + "newCategory = ["
-                + newCategory
-                + "]");
-        ArrayAdapter<String> categoriesAdapter =
-                new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, android.R.id.text1);
-        categoriesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mCategoriesSpinner.setAdapter(categoriesAdapter);
-        categoriesAdapter.add(getString(R.string.category_uncategorized));
-        // The add new category position has to match the value of: ADD_NEW_CATEGORY_POSITION
-        categoriesAdapter.add(getString(R.string.category_add_new));
-
-        Set<String> categories = mLinkStorage.getCategories();
-        if (!TextUtils.isEmpty(newCategory)) {
-            categories.add(newCategory);
-        }
-        for (String category : categories) {
-            categoriesAdapter.add(category);
-        }
-        categoriesAdapter.notifyDataSetChanged();
-
-        String currentCategory = mIntentLink.getCategory();
-        if (!TextUtils.isEmpty(currentCategory)) {
-            mCategoriesSpinner.setSelection(
-                    categoriesAdapter.getPosition(currentCategory.toLowerCase()));
-        }
-        if (!TextUtils.isEmpty(newCategory)) {
-            mCategoriesSpinner.setSelection(
-                    categoriesAdapter.getPosition(newCategory.toLowerCase()));
-        }
     }
 
     private void updateLink(final Link link) {
@@ -239,94 +186,12 @@ public class EditLinkActivity extends BaseLinkActivity
                 return;
             }
 
-            String category = mOldLink.getCategory();
-            if (mCategoriesSpinner.getSelectedItem() != null) {
-                String spinnerCategory = mCategoriesSpinner.getSelectedItem().toString();
-                if (!spinnerCategory.equalsIgnoreCase(getString(R.string.category_add_new))
-                        && !spinnerCategory.equalsIgnoreCase(
-                        getString(R.string.category_uncategorized))) {
-                    category = spinnerCategory;
-                }
-            }
-
-            if (category == null) category = "";
-            Link editedLink = new Link(mOldLink.getLinkId(), category, mArchivedSwitch.isChecked(),
-                    mFavoriteSwitch.isChecked(), mOldLink.getTimestamp(),
-                    mTitleEditText.getText().toString(), mUrlEditText.getText().toString());
+            Link editedLink = new Link(mOldLink.getLinkId(), mCategoryEditText.getText().toString(),
+                    mArchivedSwitch.isChecked(), mFavoriteSwitch.isChecked(),
+                    mOldLink.getTimestamp(), mTitleEditText.getText().toString(),
+                    mUrlEditText.getText().toString());
 
             updateLink(editedLink);
-        }
-    }
-
-    private class CategoryDialog extends Dialog implements DialogInterface.OnCancelListener {
-        private EditText mNewCategoryEditText;
-
-        public CategoryDialog(Context context) {
-            super(context);
-            dialogSetup();
-        }
-
-        @Override
-        public void onCancel(DialogInterface dialogInterface) {
-            Log.v(LOG_TAG,
-                    "onCancel() called with: " + "dialogInterface = [" + dialogInterface + "]");
-            handleCancel();
-        }
-
-        private void dialogSetup() {
-            setTitle(R.string.add_new_category_title);
-            setContentView(R.layout.add_new_category_dialog);
-            mNewCategoryEditText = (EditText) findViewById(R.id.add_new_category_name);
-            Button saveButton = (Button) findViewById(R.id.add_new_category_save_button);
-            Button cancelButton = (Button) findViewById(R.id.add_new_category_cancel_button);
-
-            if (saveButton != null) saveButton.setOnClickListener(handleSaveButton());
-            if (cancelButton != null) cancelButton.setOnClickListener(handleCancelButton());
-            setOnCancelListener(CategoryDialog.this);
-        }
-
-        @Nullable
-        private String getNewCategory() {
-            if (mNewCategoryEditText == null) return null;
-            return mNewCategoryEditText.getText().toString().trim().toLowerCase();
-        }
-
-        private void handleCancel() {
-            Log.v(LOG_TAG, "handleCancel()");
-            populateCategoriesSpinner(null);
-            dismiss();
-        }
-
-        private View.OnClickListener handleCancelButton() {
-            return new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Log.v(LOG_TAG,
-                            "handleCancelButton onClick() called with: " + "view = [" + view + "]");
-                    handleCancel();
-                }
-            };
-        }
-
-        private View.OnClickListener handleSaveButton() {
-            return new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Log.v(LOG_TAG,
-                            "handleSaveButton onClick() called with: " + "view = [" + view + "]");
-                    String category = getNewCategory();
-                    Log.i(LOG_TAG, "handleSaveButton onClick: New category: " + category);
-                    if (TextUtils.isEmpty(category)) {
-                        if (mNewCategoryEditText != null) {
-                            mNewCategoryEditText.setError(
-                                    getString(R.string.add_new_category_empty_error));
-                        }
-                        return;
-                    }
-                    populateCategoriesSpinner(category);
-                    dismiss();
-                }
-            };
         }
     }
 }
