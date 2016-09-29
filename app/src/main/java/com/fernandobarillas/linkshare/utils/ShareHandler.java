52ea990ed0c99e935018971306d236d0da3f454b
Fernando Barillas <fbis251@mailbox.org>
2016-09-29 15:46:13 -0700

    ShareHandler: Use bottom sheet for share menu
    * Mostly for pre-L devices, this won't use the native share UI anymore

--- app/src/main/java/com/fernandobarillas/linkshare/utils/ShareHandler.java
+++ app/src/main/java/com/fernandobarillas/linkshare/utils/ShareHandler.java
@@ -2,9 +2,9 @@ package com.fernandobarillas.linkshare.utils;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 
 import com.fernandobarillas.linkshare.R;
+import com.kennyc.bottomsheet.BottomSheet;
 
 /**
  * Created by fb on 9/2/16.
@@ -21,9 +21,9 @@ public class ShareHandler {
         sendIntent.putExtra(Intent.EXTRA_TEXT, url);
 
         String shareTitle = context.getString(R.string.share);
-        PackageManager packageManager = context.getPackageManager();
-        if (sendIntent.resolveActivity(packageManager) != null) {
-            context.startActivity(Intent.createChooser(sendIntent, shareTitle));
+        BottomSheet share = BottomSheet.createShareBottomSheet(context, sendIntent, shareTitle);
+        if (share != null) {
+            share.show();
             return true;
         }
         return false;
