b1bcac6822c519b4f65f206f56073c5094dbbb25
Fernando Barillas <fbis251@mailbox.org>
2017-03-08 18:58:31 -0800

    Refactor ResponsePrinter to ResponseUtils

--- app/src/main/java/com/fernandobarillas/linkshare/utils/ResponseUtils.java
+++ app/src/main/java/com/fernandobarillas/linkshare/utils/ResponseUtils.java
@@ -5,12 +5,22 @@ import retrofit2.Response;
 /**
  * Created by fb on 2/1/16.
  */
-public class ResponsePrinter {
-    public static String httpCodeString(Response response) {
+public class ResponseUtils {
+    // HTTP Status Codes
+    private static final int STATUS_UNAUTHORIZED = 401;
+
+    public static String httpCodeString(final Response response) {
         if (response == null) {
             return "";
         }
 
         return String.format("%d %s", response.code(), response.message());
+    }
+
+    public static boolean isAuthenticationError(final Response response) {
+        if (response == null) {
+            return false;
+        }
+        return response.code() == STATUS_UNAUTHORIZED;
     }
 }
